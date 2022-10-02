956da51bce63f73a4f43a7ad25e64d44e11f531c
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-02 23:44:44 +0000

    Adding tarfile member sanitization to extractall()

--- src/preprocessing/pixz.py
+++ src/preprocessing/pixz.py
@@ -36,7 +36,26 @@ def extract_pcap(archive, filename, out_directory):
     tar_path = os.path.join(out_directory, "pixz.tar")
     subprocess.call(["pixz", "-x", filename, "-i", archive, "-o", tar_path])
     with tarfile.open(tar_path) as tar:
-        tar.extractall(path=out_directory)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tar, path=out_directory)
     os.remove(tar_path)
 
     # rename to *.pcap
